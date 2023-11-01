189230bd0d409942294148d62daeba5991938ce5
Aditya Saini <adityasaini10012001@outlook.com>
2023-11-01 10:52:10 +0530

    Initial code
    Signed-off-by: Aditya Saini <adityasaini10012001@outlook.com>

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      Built by <a href="https://github.com/SainiAditya1">Aditya Saini</a>. Find the
+      source code{" "}
+      <a href="https://github.com/SainiAditya1/QuickSell_Assignment">
+        here.
+      </a>
+    </div>
+  );
+};
+
+export default Footer;

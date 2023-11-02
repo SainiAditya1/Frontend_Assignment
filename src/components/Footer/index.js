d9bd892600d9b76f6ec14f6071c19625405de523
Aditya Saini <adityasaini10012001@outlook.com>
2023-11-02 09:19:57 +0530

    minor changes
    Signed-off-by: Aditya Saini <adityasaini10012001@outlook.com>

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { FaLinkedin, FaGithub} from 'react-icons/fa6';
 import "./Footer.css";
 
 const Footer = () => {
+    const socials = [{
+      name: 'LinkedIn',
+      link: 'https://www.linkedin.com/in/aditya-saini-ba44261b8/',
+      icon: <FaLinkedin />,
+      hoverColor: 'hover:text-blue-800',
+      darkhoverColor: 'dark:hover:text-blue-800',
+    },
+    {
+      name: 'Github',
+      link: 'https://github.com/SainiAditya1',
+      icon: <FaGithub />,
+      hoverColor: 'hover:text-black',
+      darkhoverColor: 'dark:hover:text-white',
+    },
+];
   return (
     <div className="Footer">
       Built by <a href="https://github.com/SainiAditya1">Aditya Saini</a>. Find the
@@ -9,6 +25,13 @@ const Footer = () => {
       <a href="https://github.com/SainiAditya1/QuickSell_Assignment">
         here.
       </a>
+
+      
+
+
+
+
+
     </div>
   );
 };
